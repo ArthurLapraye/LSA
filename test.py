7561e614c430b124e6ccb9b4a18799670c2d9b97
Arthur Lapraye <arthurlapraye@yahoo.fr>
2016-04-13 12:09:30 +0200

    Dictionary for LSA of survey content

--- test.py
+++ test.py
@@ -4,29 +4,30 @@
 import openpyxl as xl
 import Tkinter as t
 
-root = t.Tk()
-wb = xl.load_workbook("QO10 - Copie.xlsx", guess_types=True)
+wb = xl.load_workbook("../QO10 - Copie.xlsx", guess_types=True)
 
+
+#root = t.Tk()
 #text = t.Text(root)
-scrollbar = t.Scrollbar(root)
-scrollbar.pack( side = t.RIGHT, fill=t.Y )
+#scrollbar = t.Scrollbar(root)
+#scrollbar.pack( side = t.RIGHT, fill=t.Y )
+#scroll2 = t.Scrollbar(root,orient=t.HORIZONTAL)
+#scroll2.pack( side = t.BOTTOM, fill=t.X )
 
-scroll2 = t.Scrollbar(root,orient=t.HORIZONTAL)
-scroll2.pack( side = t.BOTTOM, fill=t.X )
 
+#text = t.Listbox(root, yscrollcommand = scrollbar.set, xscrollcommand=scroll2.set )
 
-text = t.Listbox(root, yscrollcommand = scrollbar.set, xscrollcommand=scroll2.set )
+corpus=dict()
 
 for row in wb['A1']:
-	text.insert(t.END, u" | ".join([ unicode(cell.value)  for cell in row])+"\n")
+	#text.insert(t.END, u" | ".join([ unicode(cell.value)  for cell in row])+"\n")
+	corpus[row[1].value]=row[2].value
 	
-text.pack( side = t.LEFT, fill = t.BOTH, expand=1 )
-
-scroll2.config( command = text.xview )
-
-scrollbar.config( command = text.yview )
-
-
+	
+	
+	
+#text.pack( side = t.LEFT, fill = t.BOTH, expand=1 )
+#scroll2.config( command = text.xview )
+#scrollbar.config( command = text.yview )
 #text.pack()
-
-root.mainloop()
+#root.mainloop()
