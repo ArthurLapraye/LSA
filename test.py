5517e8a41b4682099c05bd96ecd829d2302eb406
Arthur Lapraye <arthurlapraye@yahoo.fr>
2016-04-13 11:17:53 +0200

    Projet LSA 2016

---
+++ test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+#-*- encoding: utf-8 -*-
+
+import openpyxl as xl
+import Tkinter as t
+
+root = t.Tk()
+wb = xl.load_workbook("QO10 - Copie.xlsx", guess_types=True)
+
+#text = t.Text(root)
+scrollbar = t.Scrollbar(root)
+scrollbar.pack( side = t.RIGHT, fill=t.Y )
+
+scroll2 = t.Scrollbar(root,orient=t.HORIZONTAL)
+scroll2.pack( side = t.BOTTOM, fill=t.X )
+
+
+text = t.Listbox(root, yscrollcommand = scrollbar.set, xscrollcommand=scroll2.set )
+
+for row in wb['A1']:
+	text.insert(t.END, u" | ".join([ unicode(cell.value)  for cell in row])+"\n")
+	
+text.pack( side = t.LEFT, fill = t.BOTH, expand=1 )
+
+scroll2.config( command = text.xview )
+
+scrollbar.config( command = text.yview )
+
+
+#text.pack()
+
+root.mainloop()
