206210932648e30e3be2936db3d2a6dd9d34f0fe
Arthur Lapraye <arthurlapraye@yahoo.fr>
2016-06-01 18:28:11 +0200

    can't stop the signal

--- main.py
+++ main.py
@@ -15,32 +15,52 @@ _fromUtf8 = QtCore.QString.fromUtf8
 
 
 class Table(QtGui.QTableWidget):
-	def __init__(self,row_count,column_count,sheet):
+	def __init__(self,sheet):
+		self.sheet=sheet
+		
+		row_count = sheet.max_row + 1
+		column_count = sheet.max_column + 1
+		
 		super(Table,self).__init__(row_count,column_count)
-		 
-		 			
-		r,x=0,0
+	 		
+		r,x=1,1
 		for row in sheet:
-			x=0
+			x=1
 			for fn in row:
 				if fn.value:
 					newitem=QtGui.QTableWidgetItem( unicode(fn.value))
 				else:
 					newitem=QtGui.QTableWidgetItem("")
-					self.setItem(r,x,newitem)
-					x += 1
 				
-				r += 1
-	
+				self.setItem(r,x,newitem)
+				
+				x += 1
+				
+			r += 1
+		
+			
+		
+		self.cellChanged.connect(self.changeItem)
+		
 	def __getitem__(self,pair):
 		x,y=pair
-		return sheet[x][y]
+		return sheet.cell(row=x, column=y)
 	
 	def __setitem__(self,pair,value):
 		x,y=pair
-		sheet[x][y]=value
-		item=self.itemAt(row, column)
-		item.value = sheet[x][y]		
+		print type(value)
+		print x,y
+		item=self.itemAt(x, y)
+		print item.text(),self.sheet.cell(row=x, column=y).value
+		
+		self.sheet.cell(row=x, column=y).value = value
+		
+		item.setText(value)		
+		print item.text(),self.sheet.cell(row=x, column=y).value
+		
+	def changeItem(self,row,col):
+		item=self.itemAt(row,col)
+		self[row,col] = unicode(item.text())
 
 class Main(QtGui.QMainWindow):
 	
@@ -93,23 +113,15 @@ class Main(QtGui.QMainWindow):
 	def openfile(self, filename):
 			if filename.endswith(".xlsx"):
 				
-				wb = opx.load_workbook(filename, guess_types=True)
+				wb = opx.load_workbook(filename, guess_types=False)
 		
 				for sheet in wb:
 					
-					row_count = sheet.max_row - 1
-					column_count = sheet.max_column + 1
-					
-					
-					self.tabtable[sheet.title] = Table(row_count, column_count, sheet)
+					self.tabtable[sheet.title] = Table(sheet)
 					self.tabs.addTab(self.tabtable[sheet.title],sheet.title)
-					
-					self.show()
-						
-					self.tabtable[sheet.title].show()
-					
-				
+									
 				self.setWindowTitle(os.path.basename(filename) )
+				self.show()
 			
 			else:
 				QMessageBox.about(self, "Erreur","Format de fichier non pris en charge.")
