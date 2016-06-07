10c44386e03d02b35c49e1456a67060073b0393c
Arthur Lapraye <arthurlapraye@yahoo.fr>
2016-06-07 23:43:51 +0200

    sécession du toklemmatiseur

---
+++ lemmtok.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+#-*- encoding: utf-8 -*-
+#Copyright Arthur Lapraye 2016
+
+import csv
+import logging
+from unidecode import unidecode
+
+
+class Lemmtok(object):
+	def __init__(self,LEFFFPATH):
+	
+		self.stoplist=set([u"","","",u"a","a",u"as","as",u"ai","ai",u"au","au",u"aux","aux",u"avec","avec",u"avoir","avoir",u"c","c",u"c'est","c'est",u"ça","ça",u"ca","ca",u"ces","ces",u"ce","ce",u"cela","cela",u"cet","cet",u"ci","ci",u"cette","cette",u"comme","comme",u"comment","comment",u"cln","cln",u"clr","clr",u"cla","cla",u"cld","cld",u"d","d","d",u"d'","d'",u"dans","dans",u"de","de",u"des","des",u"du","du",u"en","en",u"est","est",u"et","et",u"faire","faire",u"fait","fait",u"il","il",u"j","j",u"je","je",u"j","j",u"l","l","lui",u"la","la",u"là","là",u"le","le",u"les","les",u"lf","lf",u"m","m",u"mon","mon",u"me","me",u"ma","ma",u"mais","mais",u"moi","moi",u"n","n",u"ne","ne",u"on","on",u"ont","ont",u"ou","ou",u"où","où",u"parce","parce",u"plus","plus",u"pas","pas",u"pour","pour",u"par","par",u"qu","qu",u"que","que",u"qui","qui",u"quot","quot",u"r","r",u"sur","sur",u"s","s",u"sa","sa",u"se","se",u"sep","sep",u"si","si",u"son","son",u"suis","suis",u"très","très",u"un","un",u"une","une",u"y","y",u"à","à",u"ça","ça",u"été","été",u"être",u"vous"])
+		
+		self.lemmatiseur=dict()
+		self.formes=dict()
+		l2=dict()
+
+		with open(LEFFFPATH) as lexique:
+			lefff=csv.reader(lexique,delimiter="\t",quotechar=None)
+			for x in lefff:
+				if x[0] not in self.stoplist and x[2] not in self.stoplist:
+					forme=x[0].decode("utf-8")
+					cat= x[1].decode("utf-8")
+					lemme=x[2].decode("utf-8")+"\t"+cat
+					if forme in self.lemmatiseur:
+						self.lemmatiseur[forme].add(lemme)
+					else:
+						self.lemmatiseur[forme]={lemme}
+					if lemme in self.formes:
+							self.formes[lemme].add(forme)
+			
+		for u in self.lemmatiseur:
+			sansaccent=unidecode(u)
+			if sansaccent not in self.lemmatiseur:
+				if sansaccent not in l2:
+					l2[sansaccent]=self.lemmatiseur[u]
+				else:
+					l2[sansaccent].update(self.lemmatiseur[u])
+				
+		self.lemmatiseur.update(l2)
+
+		logging.info("Lemmatiseur fini de charger !")
+	
+		
+	def tokenize(self,corpus):
+	
+		tok=re.compile(u"[#*+\[\]_\" &*,;:.'^?!\/)(><-]+",flags=re.UNICODE)
+		texts=list()
+		
+		lexicon=defaultdict(float)
+			
+		for x in corpus:
+			elem=[]
+			corpus[x]=re.sub("[0-9]+"," \1 ",corpus[x],0)
+			for word in tok.split(corpus[x].lower()):
+				if word not in self.stoplist:
+					if word in self.lemmatiseur:
+						if self.lemmatiseur[word] not in self.stoplist:
+							elem += self.lemmatiseur[word]
+					else:
+						elem.append(word)
+						lexicon[word] += 1
+			
+			texts.append(elem)
+			
+		return texts
+
+	def index(corpus):
+		pass
+
+if __name__=="__main__":
+	import sys
+	LEFFFPATH=sys.argv[1]
+	lt=Lemmtok(LEFFFPATH)
+	while 1:
+		try:
+			print lt.lemmatiseur[raw_input(">>>").decode("utf-8")]
+		except KeyError:
+			print "Erreur : mot inconnu"
+		except EOFError:
+			print
+			break
